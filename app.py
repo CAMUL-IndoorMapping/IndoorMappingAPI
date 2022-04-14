eb31cd1e9bbbe33cf54785561c81d2f0098e38f9
André Morais <48691769+moraisandreua@users.noreply.github.com>
2022-04-14 19:13:23 +0100

    tutorial done

--- app.py
+++ app.py
@@ -1,9 +1,77 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+import mysql.connector
+import os
+from decouple import config
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+def db_connection():
+  mydb = mysql.connector.connect(
+    host=config('DB_HOST'),
+    user=config('DB_USER'),
+    password=config('DB_PASSWORD'),
+    database=config('DB_DATABASE')
+  )
+  mycursor = mydb.cursor()
+
+  return {"mydb":mydb, "mycursor":mycursor}
+
+@app.route("/<name>")
+def hello(name):
+  obj1={"key":"value", "key2":"valye2"}
+  obj3={}
+  obj2={}
+
+  lista = [1,2,3,4,5]
+  lista2 = ["oal", "adeus", "byebye"]
+  lista3 = [obj1, obj2, obj3]
+  lista4 = [lista, lista2, lista3]
+
+  for x in lista:
+    print(x)
+
+  for x in lista4:
+    for y in x:
+      print(y)
+
+  lista = [x for x in range(1,6)] #list compreension
+  print(lista)
+
+  return jsonify(obj1)
+
+@app.route("/beacons/<id>")
+def beacon(id):
+  #conection to database
+  return jsonify({""})
+
+@app.route("/beacons", methods=["POST"])
+def beaconInsert():
+  nome = request.json["nome"]
+
+  print(nome)
+  #conection to database
+  return "criei o beacon " + nome
+
+@app.route("/2", methods=["POST"])
+def hello2():
+  return "Hello World 2!"
+
+@app.route("/reviews", methods=["GET"])
+def getReviews():
+  db_obj=db_connection()
+  mydb=db_obj["mydb"]
+  mycursor=db_obj["mycursor"]
+
+  mycursor.execute("SELECT user.name, review.text FROM review INNER JOIN user ON review.idUser=user.id")
+
+  myresult = mycursor.fetchall()
+
+  retorno=[]
+  for x in myresult:
+    retorno.append({"userName":x[0], "review":x[1]})
+
+  return jsonify(retorno)
 
 if __name__ == "__main__":
   app.run()
